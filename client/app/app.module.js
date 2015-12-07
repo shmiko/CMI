e31e2a1ff2e58e13a4033f0a7fab1e61fcccfb52
Paul Jones <shmiko@gmail.com>
2015-12-07 23:12:32 +1100

    css add event

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -105,4 +105,23 @@
         .accentPalette('indigo'); 
       });
 
+      app.directive('scrollOnClick', function() {
+        return {
+          restrict: 'A',
+          link: function(scope, $elm, attrs) {
+            var idToScroll = attrs.href;
+            $elm.on('click', function() {
+              var $target;
+              if (idToScroll) {
+                $target = $(idToScroll);
+              } else {
+                $target = $elm;
+              }
+              $("body").animate({scrollTop: $target.offset().top}, "slow");
+            });
+          }
+        }
+      });
+
 }());
+
